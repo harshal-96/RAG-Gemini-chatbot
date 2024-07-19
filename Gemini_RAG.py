5dd924bac7e88083ead6fef69530286d6f8f33d1
harshal-96 <128535839+harshal-96@users.noreply.github.com>
2024-07-19 14:10:51 +0530

    Update Gemini_RAG.py
    Updated as per latest version of frameworks

--- Gemini_RAG.py
+++ Gemini_RAG.py
@@ -4,7 +4,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -13,7 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
 
 
 def get_pdf_text(pdf_docs):
